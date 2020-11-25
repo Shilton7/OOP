fea1efee4b5c42e5a4c87b860870a1ce76a96c0a
shilton <shilton.santiago@animaeducacao.com.br>
2020-11-25 20:36:35 -0300

    New Class

--- ByteBank/01-ByteBank/Program.cs
+++ ByteBank/01-ByteBank/Program.cs
@@ -6,7 +6,20 @@ namespace _01_ByteBank
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ContaCorrente contaDaGabriela = new ContaCorrente();
+
+            contaDaGabriela.titular = "Shilton";
+            contaDaGabriela.agencia = 771;
+            contaDaGabriela.numero = 15454;
+            contaDaGabriela.saldo = 100;
+
+            Console.WriteLine("Titular: " + contaDaGabriela.titular);
+            Console.WriteLine("Agência: " + contaDaGabriela.agencia);
+            Console.WriteLine("Conta: " + contaDaGabriela.numero);
+            Console.WriteLine("Saldo: " + contaDaGabriela.saldo);
+
+            Console.ReadLine();
+
         }
     }
 }
