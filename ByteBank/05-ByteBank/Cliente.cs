2af7398c883d45b531bf920e5fc4374fac85c543
shilton <shilton.santiago@animaeducacao.com.br>
2020-11-26 18:39:30 -0300

    Namespaces e composition

---
+++ ByteBank/05-ByteBank/Cliente.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _05_ByteBank
+{
+    public class Cliente
+    {
+        public string nome;
+        public string cpf;
+        public string profissao;
+        public int idade;
+
+    }
+}
---
+++ ByteBank/05-ByteBank/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _05_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente cliente = new Cliente();
+
+            cliente.nome = "Shilton";
+            cliente.cpf = "123.456.789-09";
+            cliente.profissao = "Dev";
+
+            ContaCorrente conta = new ContaCorrente();
+
+            conta.titular = cliente;
+            conta.agencia = 21234;
+            conta.numero = 212;
+            conta.saldo += 71;
+
+            Console.WriteLine("Nome: " + conta.titular.nome);
+            Console.WriteLine("Cpf: " + conta.titular.cpf);
+            Console.WriteLine("Profissao: " + conta.titular.profissao);
+            Console.WriteLine("Agencia: " + conta.agencia);
+            Console.WriteLine("Numero: " + conta.numero);
+            Console.WriteLine("Saldo: " + conta.saldo);
+            Console.WriteLine("Idade: " + conta.titular.idade);
+
+            Console.ReadLine();
+
+        }
+    }
+}
