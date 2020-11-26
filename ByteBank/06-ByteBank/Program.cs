af072acfc1196ffb53dff4bf4f442ae58335f4c1
shilton <shilton.santiago@animaeducacao.com.br>
2020-11-26 20:39:05 -0300

    Get and Set Properties

---
+++ ByteBank/06-ByteBank/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _06_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente();
+
+            conta.Saldo = 71;
+            Console.WriteLine(conta.Saldo);
+
+
+            Cliente cliente = new Cliente();
+
+            cliente.Nome = "Shilton";
+            cliente.Idade = 70;
+            conta.Titular = cliente;
+
+            Console.WriteLine(cliente.Nome);
+            Console.WriteLine(conta.Titular.Nome);
+
+            Console.ReadLine();
+        }
+    }
+}
