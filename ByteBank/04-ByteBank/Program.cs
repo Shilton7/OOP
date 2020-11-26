c08e1a3fcfcd3820f46fdbe46c84c27c8dcfd251
shilton <shilton.santiago@animaeducacao.com.br>
2020-11-26 17:17:38 -0300

    Functions, methods and arguments

---
+++ ByteBank/04-ByteBank/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _04_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaShilton = new ContaCorrente();
+
+            Console.WriteLine("Saldo Anterior do Shilton " + contaShilton.saldo);
+
+            bool resultado = contaShilton.Sacar(20);
+
+            Console.WriteLine("Resultado do saque do Shilton: " + resultado);
+            Console.WriteLine("Saldo Atual do Shilton: " + contaShilton.saldo);
+
+            contaShilton.Depositar(71);
+            Console.WriteLine("Saldo atualizado após depositar do Shilton: " + contaShilton.saldo);
+
+            ContaCorrente contaFabio = new ContaCorrente();
+
+            bool statusResultadoTransf = contaShilton.Transferir(11, contaFabio);
+            Console.WriteLine("Status da transferência do Shilton: " + statusResultadoTransf);
+            Console.WriteLine("Saldo Atual do Shilton: " + contaShilton.saldo);
+
+            Console.WriteLine("Saldo Atual da conta do Fabio, após a transferência feita pelo Shilton: " + contaFabio.saldo);
+
+            Console.ReadLine();
+        }
+    }
+}
