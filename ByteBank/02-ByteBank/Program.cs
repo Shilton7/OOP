fd08efb2cd8381407b3871f92b7c9ea2e6a66e2c
shilton <shilton.santiago@animaeducacao.com.br>
2020-11-25 21:19:06 -0300

    Reference types and value types

---
+++ ByteBank/02-ByteBank/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _02_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ContaCorrente conta = new ContaCorrente();
+
+            conta.titular = "Shilton";
+
+            Console.WriteLine("Titular: " + conta.titular);
+            Console.WriteLine("Saldo em reais: R$ " + conta.saldo);
+        }
+    }
+}
---
+++ ByteBank/03-ByteBank/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _03_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDoShilton = new ContaCorrente();
+
+            contaDoShilton.titular = "Shilton";
+            contaDoShilton.agencia = 881;
+            contaDoShilton.numero = 121545;
+
+            ContaCorrente contaDoShiltonLopes = new ContaCorrente();
+
+            contaDoShiltonLopes.titular = "Shilton";
+            contaDoShiltonLopes.agencia = 881;
+            contaDoShiltonLopes.numero = 121545;
+
+            //Tipo de refências diferentes
+            Console.WriteLine(contaDoShilton == contaDoShiltonLopes);
+
+            //Tipo de valor iguais
+            Console.WriteLine(contaDoShilton.agencia == contaDoShiltonLopes.agencia);
+
+            //Tipo de refências iguais
+            contaDoShilton = contaDoShiltonLopes;
+            Console.WriteLine(contaDoShilton == contaDoShiltonLopes);
+
+            //Tipo de refêrencia sendo compartilhados no mesmo objeto na memoria
+            contaDoShilton.saldo = 300;
+            Console.WriteLine(contaDoShiltonLopes.saldo); //300
+            Console.WriteLine(contaDoShilton.saldo); //300
+
+            Console.ReadLine();
+
+        }
+    }
+}
