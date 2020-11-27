f85ef99ca7146d847b44ded8adaf193521c13d30
shilton <shilton.santiago@animaeducacao.com.br>
2020-11-26 23:49:08 -0300

    Constructors and static members

---
+++ ByteBank/07-ByteBank/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _07_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+
+            ContaCorrente conta = new ContaCorrente(123,12387459);
+
+            Console.WriteLine(conta.Agencia);
+            Console.WriteLine(conta.Numero);
+            Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+
+            Console.ReadLine();
+        }
+    }
+}
